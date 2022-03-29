35f9a1b195a2cfced7e154c8cbbe0c503de53673
December0230 <th.dat1230@gmail.com>
2022-03-29 11:20:14 +0700

    exam

---
+++ Login_Register/Login_Register/Model/Database.cs
@@ -0,0 +1,47 @@
+﻿using SQLite;
+using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Register.Model
+{
+    public  class Database
+    {
+        string path;
+        SQLiteConnection conn;
+
+        public Database ()
+        {
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+                "MyDatabase.sqlite");
+            conn = new SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+            //Create table
+            conn.CreateTable<User>();
+        }
+
+        public int Register(User user) 
+        {
+            return conn.Insert(new User()
+            {
+                Username = user.Username,
+                Password = user.Password,
+                Email = user.Email,
+            });
+        }
+
+        public bool Login(string user, string password)
+        {
+            var query = conn.Table<User>().
+                Where(t => t.Username == user && t.Password == user);
+            if (query.Count() > 0)
+                return true;
+            else 
+                return false;
+        }
+    }
+}
